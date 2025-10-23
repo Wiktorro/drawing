e4d05c1a9af7bceedc7d7492821fc81d9c07272d
Wiktorro <48845694+Wiktorro@users.noreply.github.com>
2025-10-23 09:22:41 +0200

    Refactor colorscript - use controllable fuctions

--- colortest.sh
+++ colortest.sh
@@ -1,12 +1,13 @@
 # for checking terminal colors settings
+tmp="$(seq 0 7)" # 0 TO 7 INCLUSIVELY, read without "" to keep as separate words
 printf "        "
-for b in 0 1 2 3 4 5 6 7; do printf "   4${b}m "; done
+for b in ${tmp}; do printf "   4${b}m "; done
 echo
 for f in "" 30 31 32 33 34 35 36 37; do
     for s in "" "1;"; do
         printf "%4sm" "${s}${f}"
         printf " \033[%sm%s\033[0m" "${s}${f}" "gYw "
-        for b in 0 1 2 3 4 5 6 7; do
+        for b in ${tmp}; do
             printf " \033[4%s;%sm%s\033[0m" "$b" "${s}${f}" " gYw "
         done
         echo
