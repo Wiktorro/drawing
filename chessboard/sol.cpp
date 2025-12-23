347807f3b421ae396436d92a767d9fbee09910cf
Wiktor Mis <mwiktor023@gmail.com>
2025-12-23 08:59:25 +0100

    add cpp requirements

--- chessboard/sol.cpp
+++ chessboard/sol.cpp
@@ -1,4 +1,9 @@
-//
+// draw chessboard pattern of requested size
+
+// at even sizes, there are repetitions (the cases), we need to calculate start of each line
+// #0          #0#
+// 0#          0#0
+// == #00#     #0#  == #0#0#0#0#0  
 
 
 //
